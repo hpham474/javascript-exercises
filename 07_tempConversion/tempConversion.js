7ac0567addf12ba9e16be4daecbbaf80f161d66e
hpham474 <hpham474@gmail.com>
2024-05-20 01:04:29 -0500

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farenheit = ( (9 / 5) * temp) + 32;
+  return Number(farenheit.toFixed(1));
 };
 
 // Do not edit below this line
