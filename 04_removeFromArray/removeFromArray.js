751bbfaf951828761fc5b3288e4f87a9abb68f1e
hpham474 <hpham474@gmail.com>
2024-05-20 00:27:37 -0500

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove1, remove2, remove3, remove4) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === remove1 || array[i] === remove2 || array[i] === remove3 || array[i] === remove4) {
+            array.splice(i,1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
