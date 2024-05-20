5b3304cbbf63e2ac1b5d80b2d6e3d6ccec1d1d35
hpham474 <hpham474@gmail.com>
2024-05-20 00:53:03 -0500

    complete 05_summAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    larger = 0;
+    smaller = 0;
+    if (num1 > num2) {
+        larger = num1;
+        smaller = num2;
+    }
+    else {
+        larger = num2;
+        smaller = num1;
+    }
 
+    let sum = 0;
+
+    for(let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
