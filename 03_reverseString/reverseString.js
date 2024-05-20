b2dcce88461ddcbc370fff7b428c121ae71beb0e
hpham474 <hpham474@gmail.com>
2024-05-20 00:03:45 -0500

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayString = string.split("");
+    let result = "";
+    for (let i = arrayString.length-1; i >= 0; i--) {
+        console.log(i);
+        result += arrayString[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
