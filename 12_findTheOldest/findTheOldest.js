afb8adc8995d74d3a034fba5fe57121f36313d3d
hpham474 <hpham474@gmail.com>
2024-05-22 21:33:27 -0500

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    arr.sort((a,b) => {
+        const currentYear = new Date().getFullYear();
+        const aAge = (a.yearOfDeath === undefined)
+            ? currentYear - a.yearOfBirth
+            : a.yearOfDeath - a.yearOfBirth;
+        const bAge = (b.yearOfDeath === undefined)
+            ? currentYear - b.yearOfBirth
+            : b.yearOfDeath - b.yearOfBirth;
+
+        if ( aAge > bAge ) {
+            return -1;
+        }
+        if ( aAge === bAge ) {
+            return 0;
+        }
+        if ( aAge < bAge ) {
+            return 1;
+        }
+    });
+
+    return arr[0];
 };
 
 // Do not edit below this line
