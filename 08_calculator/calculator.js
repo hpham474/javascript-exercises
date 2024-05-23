42c3d3ee669af19e72c73cf108a38921a0691ec7
hpham474 <hpham474@gmail.com>
2024-05-22 20:31:20 -0500

    correct 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,26 +7,32 @@ const subtract = function(a, b) {
 };
 
 const sum = function(arr) {
-	return arr.reduce((accumulator, total) => {
-    return accumulator + total;
+	return arr.reduce((accumulator, current) => {
+    return accumulator + current;
   }, 0)
 };
 
 const multiply = function(arr) {
-  return arr.reduce((accumulator, total) => {
-    return accumulator * total;
+  return arr.reduce((accumulator, current) => {
+    return accumulator * current;
   }, 1)
 };
 
 const power = function(a, b) {
+  /*
 	let total = 1;
   for (let i = 0; i < b; i++) {
     total *= a;
   }
+
   return total;
+  */
+
+  return Math.pow(a, b);
 };
 
 const factorial = function(a) {
+  /*
 	arr = [];
   for (let i = 1; i <= a; i++) {
     arr[i] = i;
@@ -37,6 +43,13 @@ const factorial = function(a) {
   }, 1);
 
   return factor;
+  */
+
+  let product = 1;
+  for (let i = 1; i <= a; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
