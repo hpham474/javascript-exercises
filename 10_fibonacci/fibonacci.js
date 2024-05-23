5b1f82f210ee0cf18541430c06bef6b9503c979d
hpham474 <hpham474@gmail.com>
2024-05-22 21:08:38 -0500

    remove unnessary if statements from 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,6 @@
 const fibonacci = function(n) {
     let fib = [0, 1];
-    if (n === 0) {
-        return 0;
-    } 
-    else if (n === 1) {
-        return 1;
-    }
-    else if (n < 0) {
+    if (n < 0) {
         return "OOPS";
     }
     for (let i = 2; i <= n; i++) {
