e9e7d30478d0d5a5e7a0bd80cbea33d542909eff
hpham474 <hpham474@gmail.com>
2024-05-22 20:57:59 -0500

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+    
+    cleanedString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
 
-};
+    reverseString = cleanedString
+        .split('')
+        .reverse()
+        .join('');
+
+    if(cleanedString === reverseString) {
+        return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
