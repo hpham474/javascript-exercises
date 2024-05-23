1904b5751d17e8b283a5e590dc0645493176e809
hpham474 <hpham474@gmail.com>
2024-05-22 21:05:57 -0500

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fib = [0, 1];
+    if (n === 0) {
+        return 0;
+    } 
+    else if (n === 1) {
+        return 1;
+    }
+    else if (n < 0) {
+        return "OOPS";
+    }
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
 
+    return fib[n];
 };
 
 // Do not edit below this line
